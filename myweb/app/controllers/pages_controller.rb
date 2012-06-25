b45dea28a5f074375954460c148d2222a9fc9f9c
Richard Li <lly123@gmail.com>
2012-06-25 22:40:02 +0800

    use @title

--- myweb/app/controllers/pages_controller.rb
+++ myweb/app/controllers/pages_controller.rb
@@ -1,11 +1,13 @@
 class PagesController < ApplicationController
   def home
-
+    @title = "Home Page"
   end
 
   def contact
+    @title = "Contact Page"
   end
 
   def about
+    @title = "About Page"
   end
 end
